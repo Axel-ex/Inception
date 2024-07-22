0c5e0cebf0f0466b34ec4ab9ca4d5edfcae58eb4
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2024-07-22 16:23:41 +0100

    mariadb still giving problems

--- Makefile
+++ Makefile
@@ -1,17 +1,23 @@
 PATH_TO_COMPOSE = ./srcs/docker-compose.yml
 PATH_TO_ENV_FILE = whatever
 PROJECT_NAME = inception
+VOLUMES = /Users/Axel/data_inception/
 
-all: build run
-
-build:
-	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} build
+all: run
 
 run:
 	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} up -d
+
+force:
+	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} build --no-cache
+
+run:
 
 stop:
 	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} stop
 
 clean:
 	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} down
+	rm -rf ${VOLUMES}
+
+re: clean force all
--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,15 +1,43 @@
 #!/bin/bash
-#
-mysql_install_db --user=$DB_USER --ldata=/var/lib/mysql
+
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
 service mysql start
 
-echo "DB_NAME " $DB_NAME
+# Wait for the MySQL server to be ready
+until mysqladmin ping &>/dev/null; do
+	echo "Waiting for MySQL server to be ready..."
+	sleep 3
+done
+
+# Create a temporary MySQL configuration file for initialization
+cat >/tmp/my.cnf <<EOF
+[client]
+user=root
+password=''
+EOF
+
 # Create a SQL script file and add SQL commands to it
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" >db.sql
-echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >>db.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >>db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '42rules' ;" >>db.sql
-echo "FLUSH PRIVILEGES;" >>db.sql
+cat >/tmp/db.sql <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PWD_ROOT}';
+FLUSH PRIVILEGES;
+EOF
 
 # Execute the SQL script using the MySQL client
-mysql <db.sql
+mysql --defaults-file=/tmp/my.cnf </tmp/db.sql
+
+# Update the MySQL configuration file with the new root password
+cat >/tmp/my.cnf <<EOF
+[client]
+user=root
+password='$DB_PWD_ROOT'
+EOF
+
+# Shutdown the MySQL server
+mysqladmin --defaults-file=/tmp/my.cnf shutdown
+
+# Clean up
+rm /tmp/my.cnf
+rm /tmp/db.sql
