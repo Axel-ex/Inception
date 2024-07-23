59424b1c4397a17081faddb0c445af928b7df019
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2024-07-23 13:08:31 +0100

    docker compose up keeps all container alive

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
+# Ensure the data directory is initialized
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
-service mysql start
-
-# Wait for the MySQL server to be ready
-until mysqladmin ping &>/dev/null; do
-	echo "Waiting for MySQL server to be ready..."
-	sleep 3
-done
-
-# Create a temporary MySQL configuration file for initialization
-cat >/tmp/my.cnf <<EOF
-[client]
-user=root
-password=''
-EOF
 
 # Create a SQL script file and add SQL commands to it
 cat >/tmp/db.sql <<EOF
@@ -26,18 +13,10 @@ FLUSH PRIVILEGES;
 EOF
 
 # Execute the SQL script using the MySQL client
-mysql --defaults-file=/tmp/my.cnf </tmp/db.sql
+mysql -uroot </tmp/db.sql
 
-# Update the MySQL configuration file with the new root password
-cat >/tmp/my.cnf <<EOF
-[client]
-user=root
-password='$DB_PWD_ROOT'
-EOF
-
-# Shutdown the MySQL server
-mysqladmin --defaults-file=/tmp/my.cnf shutdown
+# Stop the MySQL server
+mysqladmin -uroot -p${DB_PWD_ROOT} shutdown
 
 # Clean up
-rm /tmp/my.cnf
 rm /tmp/db.sql
