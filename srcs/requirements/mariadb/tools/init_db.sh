b454f8102b892012c272b647affc3e79eaea2118
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2024-07-26 08:40:16 +0100

    still problems with db

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 # Ensure the data directory is initialized
+echo "Initializing MariaDB data directory..."
 mysql_install_db --user=mysql --ldata=/var/lib/mysql
+echo "MariaDB data directory initialized."
+
+# Start the MySQL server
+echo "Starting MySQL server..."
+mysqld_safe &
+sleep 5
+
+# Wait for the MySQL server to be ready
+until mysqladmin ping &>/dev/null; do
+	echo "Waiting for MySQL server to be ready..."
+	sleep 3
+done
 
 # Create a SQL script file and add SQL commands to it
+echo "Creating initial SQL script..."
 cat >/tmp/db.sql <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PWD_ROOT}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PWD_ROOT';
 FLUSH PRIVILEGES;
 EOF
 
+echo "Initial SQL script created. Executing script..."
 # Execute the SQL script using the MySQL client
 mysql -uroot </tmp/db.sql
-
-# Stop the MySQL server
-mysqladmin -uroot -p${DB_PWD_ROOT} shutdown
-
-# Clean up
-rm /tmp/db.sql
+if [ $? -eq 0 ]; then
+	echo "SQL script executed successfully."
+else
+	echo "Error executing SQL script."
+	exit 1
+fi
--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-#get cli word press to automatically link with our datbase.
-wget -O wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-
 #create directory for nginx
 mkdir -p /var/www && mkdir -p /var/www/html
+cd /var/www/html
 #download latest wordpress
-wp core download --path=/var/www/html --allow-root
+wp core download --allow-root
 
-wp config create --allow-root \
-	--dbname=$DB_NAME \
-	--dbuser=$DB_USER \
-	--dbpass=$DB_PWD \
-	--dbhost=mariadb:3306 --path='/var/www/html'
+wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=mariadb:3306
 
 #Install wordpress and sets up necessary info
 wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
