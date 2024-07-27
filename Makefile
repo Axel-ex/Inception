c08f4dfbbf1f01bc3d48930c395bd8b4a0e40a12
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2024-07-27 11:09:57 +0100

    wordpress issues
    - Impossible to connect to the url but localhost url works

--- Makefile
+++ Makefile
@@ -5,18 +5,18 @@ PROJECT_NAME = inception
 all: run
 
 run:
-	sudo docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} up -d
+	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} up -d
 
 force:
-	sudo docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} build --no-cache
+	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} build --no-cache
 
 run:
 
 stop:
-	sudo docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} stop
+	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} stop
 
 clean:
-	sudo docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} down -v
+	docker compose -p ${PROJECT_NAME} --file ${PATH_TO_COMPOSE} down -v
 
 re: clean force all
 
--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -8,15 +8,23 @@ rm -rf *
 #download latest wordpress
 wp core download --allow-root
 
+# Wait for MariaDB to be ready
+while ! mysqladmin ping -hmariadb --silent; do
+	echo "Waiting for MariaDB to be ready..."
+	sleep 2
+done
+
 wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=mariadb:3306
 
 #Install wordpress and sets up necessary info
 wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 
+wp user create $DB_USER $WP_ADMIN_EMAIL --role=author --user_pass=$DB_PWD --allow-root
+
 #make php fpm listen to port 9000
-sed -i 's/^listen =.*/listen = 0.0.0.0:9000/' /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 
 #php-fpm uses this directory to store unix domain sockets
 mkdir /run/php
 
-
+exec "$@"
