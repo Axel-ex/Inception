78fc725fc8428c2506a5a2e42807648a3f5b39a6
Axel chabrerie <114242486+Axel-ex@users.noreply.github.com>
2024-07-27 12:17:11 +0100

    final

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-#create directory for nginx
-mkdir -p /var/www && mkdir -p /var/www/html
-cd /var/www/html
-rm -rf *
+# Ensure proper ownership and permissions
+chown -R www-data:www-data /var/www/*
+chmod -R 755 /var/www/*
 
-#download latest wordpress
-wp core download --allow-root
+# Create necessary directories
+mkdir -p /run/php/
+touch /run/php/php7.3-fpm.pid
 
-# Wait for MariaDB to be ready
-while ! mysqladmin ping -hmariadb --silent; do
-	echo "Waiting for MariaDB to be ready..."
-	sleep 2
-done
-
-wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=mariadb:3306
-
-#Install wordpress and sets up necessary info
-wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-wp user create $DB_USER $WP_ADMIN_EMAIL --role=author --user_pass=$DB_PWD --allow-root
-
-#make php fpm listen to port 9000
+# Make PHP-FPM listen to port 9000
 sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf"
 
-#php-fpm uses this directory to store unix domain sockets
-mkdir /run/php
+# Check if WordPress is already set up
+if [ ! -f /var/www/html/wp-config.php ]; then
+	echo "Wordpress: setting up..."
 
+	# Create directory for nginx
+	mkdir -p /var/www/html
+	cd /var/www/html
+	rm -rf *
+
+	# Download wp-cli and WordPress
+	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+	wp core download --allow-root
+
+	# Wait for MariaDB to be ready
+	while ! mysqladmin ping -hmariadb --silent; do
+		echo "Waiting for MariaDB to be ready..."
+		sleep 2
+	done
+
+	# Create wp-config.php and install WordPress
+	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PWD --dbhost=mariadb:3306
+	wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+	echo "Wordpress: setup complete!"
+fi
+
+# Execute the CMD from Dockerfile
 exec "$@"
